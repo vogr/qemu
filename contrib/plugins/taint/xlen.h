b4f9c14b655978df450639400cb128cefd511498
vogr <contact@vogier.fr>
2022-09-09 16:39:37 +0200

    Build time parameter for taint plugin: RISCV_XLEN

--- contrib/plugins/taint/xlen.h
+++ contrib/plugins/taint/xlen.h
@@ -6,12 +6,14 @@
 static_assert(RISCV_XLEN == 64 || RISCV_XLEN == 32, "RISCV_XLEN must be defined to be equal to 32 or 64!");
 
 #if RISCV_XLEN == 64
+    #define TARGET_RISCV64 1
     #define XLEN 64
     typedef uint64_t target_ulong;
     typedef int64_t target_long;
     #define TARGET_ULONG_MAX UINT64_MAX
     #define PRIxXLEN PRIx64
 #elif RISCV_XLEN == 32
+    #define TARGET_RISCV32 1
     #define XLEN 32
     typedef uint32_t target_ulong;
     typedef int32_t target_long;
